ce79e6417dc03613d38c3f609bb1f19639a17c13
meblo98 <lomouhamedelbachir@gmail.com>
2024-03-28 11:34:13 +0000

    fin des questions 1 et 2

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php
 class Voiture{
-    public $marque;
-    public $model;
-    public $kilometrage;
-    public $annee;
-    
+
+    // Propriétes
+    private $marque;
+    private $modele;
+    private $kilometrage;
+    private $annee;
+    private $couleur;
+
+    //constructeur
+    public function __construct($marque, $modele, $kilometrage, $annee,$couleur)
+    {
+        $this -> marque = $marque;
+        $this -> modele = $modele;
+        $this -> kilometrage = $kilometrage;
+        $this -> annee = $annee;
+        $this -> couleur = $couleur;
+    }
+
+    // fonction afficher les details de voiture
+    public function affiche_detail()
+    {
+        echo "Marque : ".$this -> marque."<br/>";
+        echo "modele : ".$this -> modele."<br/>";
+        echo "kilometrage : ".$this -> kilometrage."<br/>";
+        echo "annee : ".$this -> annee."<br/>";
+        echo "couleur : ".$this -> couleur;        
+        
+    }
 }
+
+    // Création de l'objet bassine de la classe Voiture
+$bassine = new Voiture("Toyota", "Prado", "0km",2020, "Rose");
+
+// on va appelé la fonction affice detail
+$bassine -> affiche_detail();
+
